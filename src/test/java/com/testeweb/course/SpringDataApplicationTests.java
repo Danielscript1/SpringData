0966840c1ec04d11f143b278476ff877e25184fb
Danielscript1 <danielsousagit@gmail.com>
2021-11-19 10:29:18 -0300

    springData

---
+++ src/test/java/com/testeweb/course/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testeweb.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
